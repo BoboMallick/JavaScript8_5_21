cf6d31f4fd12262fe23e2f9d68c3d33e6963cd0f
bobo mallick <mallickbobo@gmail.com>
2021-08-05 13:57:46 +0530

    Thanks

---
+++ 1. JS_Stack Learner/5.First code of JS/hello.js
@@ -0,0 +1,6 @@
+console.log("Hello Googler i'm Keya Mallick.");
+console.log('I Love My Job');
+console.log(2025);
+console.log('9' + 9);
+console.log(9 + 9);
+console.log('Thank you so much Universe!');
