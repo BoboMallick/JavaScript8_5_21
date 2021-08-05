30ebdcb430d2f4b53fb194fbf5ffe7307d5d0fc1
bobo mallick <mallickbobo@gmail.com>
2021-08-05 13:55:43 +0530

    variables

---
+++ 1. JS_Stack Learner/6. What is Variables/variables.js
@@ -0,0 +1,6 @@
+// What is Variables(var) = Variables is Container which can store Data.
+var name = 'Keye Mallick';
+var age = 23;
+
+console.log(name, age);
+console.log(name + ' knows JavaScript very well and her age only ' + age)
