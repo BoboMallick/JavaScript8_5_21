cdba2620099df72df052260f932978a186e54e93
bobo mallick <mallickbobo@gmail.com>
2021-08-06 17:21:20 +0530

    null vs undefined var done

---
+++ 1. JS_Stack Learner/12.JS Null & Undefined/null_vs_unde.js
@@ -0,0 +1,12 @@
+// **************************JS Null & Undefined**************************
+
+// Undefined = A variable without value always store Undefinde.
+var abc;
+console.log(abc);
+
+
+
+// Null = A variable with unknown value can store Null.
+// Null is a special type used as a absence of an object.
+var xyz = null;
+console.log(xyz);
