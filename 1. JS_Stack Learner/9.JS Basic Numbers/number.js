e8212e6c975139076488712f3d57ec708fa1cf35
bobo mallick <mallickbobo@gmail.com>
2021-08-06 17:01:04 +0530

    number var done

---
+++ 1. JS_Stack Learner/9.JS Basic Numbers/number.js
@@ -0,0 +1,11 @@
+// *******NUMBER********
+var n = 0369;
+var f = 3.69;
+var nn = Number(36.69);
+console.log(nn);
+console.log(Number.parseFloat(nn))
+console.log(Number.parseInt(nn));
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(1/0);
+console.log('abc' * 10);
