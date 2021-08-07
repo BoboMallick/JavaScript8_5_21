a5cefa1222f192f10f59fc44756f16a2a9e93ec9
bobo mallick <mallickbobo@gmail.com>
2021-08-07 10:15:09 +0530

    operators add

---
+++ 1. JS_Stack Learner/15. JS Operators/JS_Operators.js
@@ -0,0 +1,54 @@
+//********Arithmetic Operator******** = +, -, /, %, ++, --
+var a =10;
+var b = 3;
+// console.log(a % b);
+
+
+//*******Incremental (2 type)******** -> 1.Pre increment(++a), 2.Post increment(a++)
+
+console.log(++a);
+console.log(a++);
+console.log(a++);
+
+
+
+//***Assignment Operator****
+var a = 30;
+var b = 60;
+a += b;   // a = a + b
+console.log(a);
+
+a -= b;
+console.log(a)
+
+
+
+
+/* *********Comparison Operators******** */
+// Comparison values always return Boolean values
+
+var x = 30;
+var y = 60;
+console.log(x == y);   //false
+
+console.log(x != y);   //true
+
+console.log(a < b);    //true
+
+console.log(a > b);    //false
+
+console.log(a >= b);   //false
+
+console.log(a <= b);    //true
+
+var c = '30';
+var d = 30;
+console.log(c === d);  //false
+
+
+
+
+
+//Logical Operators = &&, ||, !
+
+//Bitwise Operators = &, ~, ^, <<(left shift), >>(right shift), 
