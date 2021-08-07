c95371ead9ce6912ee62acd4f36823dfdd8d0a87
bobo mallick <mallickbobo@gmail.com>
2021-08-07 08:52:51 +0530

    JS Type Conversion done

---
+++ 1. JS_Stack Learner/13.JS Type Conversion/js_type_conversion.js
@@ -0,0 +1,48 @@
+var string = '9';
+var number = 3;
+console.log(string * number);     // auto Implicit conversions for multiple.
+
+
+console.log(string + number);
+
+
+console.log(Number(string));     // String to Number convertion.
+
+
+console.log(Number.parseInt(string));    //string to integer convertion.
+
+
+console.log(number.toString());     //Number to String convertion.
+
+
+//We can not convert Infinity to Number but we can convert Infinity to String, Boolean
+console.log(Number(Infinity)); 
+
+
+
+
+
+//Falsy values
+ 0
+ null
+ undefined
+ NaN
+
+ console.log(Boolean(''));
+
+ console.log(Boolean('hgugikhkk'));
+
+ console.log(Boolean(null));
+
+ console.log(Boolean(undefined));
+
+ console.log(Boolean(0));
+
+ console.log(Boolean(369));
+
+
+
+ var x = true;
+console.log(x.toString());
+
+console.log(true);
