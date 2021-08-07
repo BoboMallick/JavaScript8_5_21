43c819e0055d122e6714b7690c494254c88d8f7a
bobo mallick <mallickbobo@gmail.com>
2021-08-07 16:03:41 +0530

    math function done

---
+++ 1. JS_Stack Learner/17. JS Math Functions/JSMathFunctions.js
@@ -0,0 +1,15 @@
+//Functions are core building blocks of any programming language. 
+
+console.log(Math.PI)
+console.log(Math.E)
+
+var number = 3.396
+console.log(Math.abs(number))
+console.log(Math.floor(number))
+console.log(Math.ceil(number))
+console.log(Math.round(number))
+console.log(Math.max(300, 600, 900))
+console.log(Math.min(300, 600, 900))
+console.log(Math.pow(6, 3))
+console.log(Math.sqrt(9))
+console.log(Math.round(Math.random() * 10 + 1))
