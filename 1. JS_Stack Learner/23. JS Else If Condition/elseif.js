94c2cda0c2fbc58e6e8d642425c0f9d8d38ca907
bobo mallick <mallickbobo@gmail.com>
2021-08-08 07:20:16 +0530

    elseif done

---
+++ 1. JS_Stack Learner/23. JS Else If Condition/elseif.js
@@ -0,0 +1,12 @@
+var a = 30
+var b = 30
+
+if(a > b){
+    console.log('a is bigger than b')
+}
+else if(a < b){
+    console.log('a is smaller than b')
+}
+else{
+    console.log('They both are same')
+}
