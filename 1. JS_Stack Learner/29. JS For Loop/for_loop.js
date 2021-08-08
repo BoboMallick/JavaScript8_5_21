c798d08f2862649aa40dc0dec12906ddc86cde15
bobo mallick <mallickbobo@gmail.com>
2021-08-08 11:00:44 +0530

    for loop 1st part

---
+++ 1. JS_Stack Learner/29. JS For Loop/for_loop.js
@@ -0,0 +1,25 @@
+//  for(var i = 0(initializer); i < 100 (condition); i++ (increment)){
+
+//     console.log('Keya Mallick')
+// }
+
+
+
+
+// for (var i = 10; i < 10; i++){
+//     console.log((i) + ' Keya Mallick')
+// }
+
+
+// for (var i = 100; i < 1000; i+= 100){
+//     console.log((i) + ' Keya Mallick')
+// }
+
+
+
+
+for (var i = 1; i <= 100; i++){
+    if(i % 2 === 0){
+        console.log(i)
+    }
+}
