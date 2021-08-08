25d7d2a120f5c106011a5f66fa1e7cea89cc5bed
bobo mallick <mallickbobo@gmail.com>
2021-08-08 19:07:50 +0530

    even number sum help of for loop

--- 1. JS_Stack Learner/29. JS For Loop/for_loop.js
+++ 1. JS_Stack Learner/29. JS For Loop/for_loop.js
@@ -28,10 +28,13 @@
 
 
 
-var n = 0;
+var sum = 0;
 
-for(var i = 0; i < 100; i++){
+for(var i = 1; i <= 10; i++){
     if(i % 2 === 0){
-        console.log(n+i)
+        console.log(sum + '+' + i  + '=' + (sum + i));
+        sum += i
     }
 }
+
+console.log('result: ' + sum)
