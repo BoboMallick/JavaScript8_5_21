ad6dfd45aeb730d03554ded90d5743912eff19df
bobo mallick <mallickbobo@gmail.com>
2021-08-08 19:10:05 +0530

    1 to 2 number sum help of for loop

--- 1. JS_Stack Learner/29. JS For Loop/for_loop.js
+++ 1. JS_Stack Learner/29. JS For Loop/for_loop.js
@@ -31,10 +31,9 @@
 var sum = 0;
 
 for(var i = 1; i <= 10; i++){
-    if(i % 2 === 0){
         console.log(sum + '+' + i  + '=' + (sum + i));
         sum += i
-    }
+    
 }
 
 console.log('result: ' + sum)
