623a5316397eb2c2b175eea4a22078d555faaf49
bobo mallick <mallickbobo@gmail.com>
2021-08-08 08:32:51 +0530

    ternart operator done

---
+++ 1. JS_Stack Learner/26.  JS Ternary Operator/ternary.js
@@ -0,0 +1,16 @@
+// var n = 3
+// var str = ''
+
+// if (n % 2 === 0) {
+//     str = 'Even'
+// }
+// else{
+//     str = 'Odd'
+// }
+// console.log(str)
+
+
+//Condition ? true side : false side
+var n = 6
+var result = n % 2 == 0 ? true : false
+console.log(result)
