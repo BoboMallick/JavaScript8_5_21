6d4b63aa0b59c532521258606af5f055951c606c
bobo mallick <mallickbobo@gmail.com>
2021-08-09 10:20:00 +0530

    do while loop

---
+++ 1. JS_Stack Learner/31. JS Do While Loop/do_while.js
@@ -0,0 +1,15 @@
+// While Loop
+
+// var isRan = false;
+// while (isRan) {
+//     console.log('O am ranning')
+// }
+
+
+// DO While Loop
+
+var isRan = false;
+
+do{
+    console.log('I am ranning')
+}while(isRan)
