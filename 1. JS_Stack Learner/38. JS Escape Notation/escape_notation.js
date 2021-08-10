39b8e1ea572ec82d8f7a70ca49408a0dcaea92e3
bobo mallick <mallickbobo@gmail.com>
2021-08-10 13:09:13 +0530

    escape notation

---
+++ 1. JS_Stack Learner/38. JS Escape Notation/escape_notation.js
@@ -0,0 +1,25 @@
+// JS Escape Notation
+
+let str = 'This is \"String\"'
+console.log(str)
+
+let str2 = 'This is \nString'
+console.log(str2)
+
+let str3 = 'This is \tString'  //tab
+console.log(str3)
+
+let str4 = 'This is \\String'  // 
+console.log(str4)
+
+let str5 = 'This is \rString' //Carriage Return
+console.log(str5)
+
+let str6 = 'This \v is String'  //Vartical 
+console.log(str6)
+
+let str7 = 'This is \bString'  //Backspace 
+console.log(str7)
+
+let str8 = 'This is \fString'  //Form Feed
+console.log(str8)
