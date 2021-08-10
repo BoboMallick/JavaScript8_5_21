0d411fab84dbfb354e9e85eeab008ec1a7eaa4a0
bobo mallick <mallickbobo@gmail.com>
2021-08-10 11:09:00 +0530

    infinity fo loop

---
+++ 1. JS_Stack Learner/35. JS Infinity for Loop/Infinity_for_loop.js
@@ -0,0 +1,12 @@
+//JS Infinity for Loop
+
+for(; ;){
+        var rand = Math.floor(Math.random() * 10 + 1)
+        if(rand === 9){
+            console.log('Winner')
+            break
+        }
+        else{
+            console.log('You have got ' + rand )
+        }
+    }
