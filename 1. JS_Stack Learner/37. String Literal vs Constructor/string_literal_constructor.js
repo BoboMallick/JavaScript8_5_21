5262eb4385f1781f37e8d7489ad293d4634a1ee4
bobo mallick <mallickbobo@gmail.com>
2021-08-10 12:06:39 +0530

    string literal vs constructor

---
+++ 1. JS_Stack Learner/37. String Literal vs Constructor/string_literal_constructor.js
@@ -0,0 +1,12 @@
+//  String Literal vs Constructor
+
+let str = 'something'   //string literal
+console.log(str)
+
+let str2 = String('something')   //string constructor
+console.log(str2)
+
+let n = 10
+let str3 = String(n) //string constructor
+console.log(str3)
+
