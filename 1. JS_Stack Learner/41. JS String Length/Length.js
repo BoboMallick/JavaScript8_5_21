f2ec8fac623ee214fb77751fb1c6dd7b5cc818d8
bobo mallick <mallickbobo@gmail.com>
2021-08-10 19:56:56 +0530

    length is done

---
+++ 1. JS_Stack Learner/41. JS String Length/Length.js
@@ -0,0 +1,18 @@
+// let str = 'some string'
+
+// var length = 0
+
+// while(true) {
+//     if(str.charAt(length) === ''){
+//         break
+//     }
+//     else{
+//         length ++
+//     }
+// }
+// console.log(length)
+
+
+
+let name = 'Keya Mallick'
+console.log(name.length)
