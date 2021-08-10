32828062d8cd82dd1194219dbfd166602f71b881
bobo mallick <mallickbobo@gmail.com>
2021-08-10 17:22:18 +0530

    string methods

---
+++ 1. JS_Stack Learner/40. JS String Methods/String_Methods.js
@@ -0,0 +1,43 @@
+// 1. Concat Methods
+
+// let a = 'Keya Mallick'
+// let b = 'is Googler 2025'
+// let c = a.concat(' ', b)
+// console.log(c)
+
+
+
+// Substr Method
+
+// let a = 'Keya Mallick'
+// let b = 'is Googler 2025'
+// let c = a.concat(' ', b)
+// let d = c.substr(5, 19)
+// console.log(d)
+
+
+
+// charAt Method
+
+// let a = 'Keya Mallick'
+// console.log(a.charAt(9))
+
+
+// startswith and endsWith  Methods
+let a = 'Keya Mallick'
+let b = 'is a Googler'
+let c = a.concat(' ', b)
+console.log(c.startsWith('Keya'))
+console.log(c.endsWith('Googler'))
+
+// upper case and lower case Methods
+console.log(a.toUpperCase())
+console.log(b.toLowerCase())
+
+
+// trim Methods
+console.log('   keya    '.trim())
+
+// split Methods
+console.log(c.split(' '))
+console.log(b.split(''))
