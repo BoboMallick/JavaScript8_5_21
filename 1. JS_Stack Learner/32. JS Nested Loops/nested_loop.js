497c7d619ed2293a9eee83afa7a9534b43621ca8
bobo mallick <mallickbobo@gmail.com>
2021-08-10 08:54:49 +0530

    nested loop done but not clear

---
+++ 1. JS_Stack Learner/32. JS Nested Loops/nested_loop.js
@@ -0,0 +1,103 @@
+// var n = 5
+
+// for (var i = 1; i <= n; i++) {
+//     result = ''
+//     for (var j = 1; j <= i; j++) {
+//       result += j + ' '  
+//     }
+//     console.log(result)
+// }
+
+
+
+// var n = 1
+
+// for (var i = 10; i >= n; i--) {
+//     console.log(i)
+// }
+
+
+
+// var n = 1
+
+// for (var i = 10; i >= n; i--) {
+//     result = ''
+//     for (var j = 10; j >= i; j--) {
+//       result = j + ' ' + result  
+//     }
+//     console.log(result)
+// }
+
+
+
+
+// var n = 5
+
+// for (var i = 1; i <= n; i++) {
+//     result = ''
+//     for (var j = 1; j <= i; j++) {
+//       result = j + ' ' + result  
+//     }
+//     console.log(result)
+// }
+
+
+
+
+
+
+
+// var n = 1
+
+// for (var i = 5; i >= n; i--) {
+//     result = '' 
+//     for (var j = 5; j >= i; j--) {
+//       result = j + ' ' + result  
+//     }
+//     console.log(result)
+// }
+
+
+
+
+
+// var n = 1
+
+// for (var i = 5; i >= n; i--) {
+//     result = '' 
+//     for (var j = i; j >= 1; j--) {
+//       result +='*' + ' '  
+//     }
+//     console.log(result)
+// }
+
+
+
+
+
+
+
+// var n = 5
+
+// for (var i = 1; i <= n; i++) {
+//     result = '' 
+//     for (var j = i; j <= 5; j++) {
+//       result +=j + ' '  
+//     }
+//     console.log(result)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
