8c33433dab2b7c1c3198ca842015c7625aff8fb5
bobo mallick <mallickbobo@gmail.com>
2021-08-11 09:06:27 +0530

    array declearation

---
+++ 1. JS_Stack Learner/43. JS Array Declaration/array.js
@@ -0,0 +1,12 @@
+let arr = [0, 3, 6, 9]
+console.log(arr)
+console.log(arr[3])
+arr[4] = 12
+// console.log(arr)
+arr[10] = 18
+// console.log(arr.length)
+arr[3] = 90
+console.log(arr)
+
+let arr2 = Array(1, 2 , 3)  //array constractor
+console.log(arr2)
