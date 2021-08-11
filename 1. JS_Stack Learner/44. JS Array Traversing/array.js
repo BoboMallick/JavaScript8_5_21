13dc8251384303614e9144749b5bd620fad18a1a
bobo mallick <mallickbobo@gmail.com>
2021-08-11 09:45:11 +0530

    array traversing

---
+++ 1. JS_Stack Learner/44. JS Array Traversing/array.js
@@ -0,0 +1,50 @@
+// let arr = [1, 7, 9, 3, 6, 7 ]
+
+// for ( var i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+
+// let arr = [1, 7, 9, 3, 6, 7 ]
+
+// for ( var i = 0; i < arr.length; i++){
+//     arr2 = arr[i] + 2
+//     // console.log(arr[i] + 2)
+//     console.log(arr2)
+// }
+
+
+
+
+// let arr = [1, 5, 9, 3, 6, 7 ]
+// let sum = 0
+// for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i]
+//     console.log(arr[i] + ' ' + sum)
+// }
+
+
+
+
+// let arr = [1, 5, 9, 3, 6, 7 ]
+// let sum = 0
+
+// for(let i = 0; i < arr.length; i++){
+//     sum += arr[i]
+// }
+// console.log(sum)
+
+
+
+
+
+// Sum of Even number 
+
+// let arr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+// let sum = 0
+// for(let i = 0; i < arr3.length; i++){
+//     if(arr3[i] % 2 === 0){
+//         sum += arr3[i]
+//         console.log(sum)
+//     }
+// }
