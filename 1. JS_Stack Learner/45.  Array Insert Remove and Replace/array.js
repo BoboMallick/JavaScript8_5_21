8b9f877419e12077a5271fd3e65fcf24c0dd272b
bobo mallick <mallickbobo@gmail.com>
2021-08-11 10:10:45 +0530

    git add and remove done

---
+++ 1. JS_Stack Learner/45.  Array Insert Remove and Replace/array.js
@@ -0,0 +1,17 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9,]
+
+// arr.push(10)              // add element to last position
+
+// arr.unshift(0)           // add element to first position
+
+// arr.splice(3, 0, 6, 9)   //add element to specific position
+
+// arr[2] = undefined       //remove element but leangth is same as before.
+
+// arr.pop()                // remove element from last position.
+
+// arr.shift()              // remove element from first position.
+
+// arr.splice(3, 1)        // remove element from specific position.
+
+console.log(arr) 
