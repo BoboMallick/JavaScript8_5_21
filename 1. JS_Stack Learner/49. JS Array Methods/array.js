b0716c339b7a91cf9df6458916c9228a391d34e6
bobo mallick <mallickbobo@gmail.com>
2021-08-12 13:27:42 +0530

    array methods

---
+++ 1. JS_Stack Learner/49. JS Array Methods/array.js
@@ -0,0 +1,24 @@
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(array.join(' $ '))
+
+
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// array.fill(0)
+// console.log(array)
+
+
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let array2 = [10, 11, 12]
+// let array3 = array.concat(array2)
+// console.log(array3)
+
+
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// console.log(Array.isArray(array))
+
+
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let array2 = Array.from(array)
+// array2[3] = 1;
+// console.log(array)
+// console.log(array2)
