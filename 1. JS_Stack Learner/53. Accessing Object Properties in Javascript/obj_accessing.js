f18d7be9232bd658ec8af3935ec8e5f977b054e7
bobo mallick <mallickbobo@gmail.com>
2021-08-14 10:19:09 +0530

    obj accessing

---
+++ 1. JS_Stack Learner/53. Accessing Object Properties in Javascript/obj_accessing.js
@@ -0,0 +1,21 @@
+
+// let point = {
+//     a : 6,
+//     b : 1,
+//     c : 4
+// }
+
+// console.log(point.a)             // Dot notation accessing method
+// console.log(point.a + point.b)   // Dot notation accessing method 
+
+// console.log(point['c'])          // Array notation accessing method
+// If we  don't know that which property or value we are going to use that time we need to Array Methods
+
+
+let point = {
+    a : 6,
+    b : 1,
+    c : 4
+}
+let show = 'a'
+console.log(point[show]) 
