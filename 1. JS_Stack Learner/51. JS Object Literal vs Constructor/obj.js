8f86ad8a23567026b5b8ff03a1c5c846921c4dd4
bobo mallick <mallickbobo@gmail.com>
2021-08-14 09:46:27 +0530

    obj Literal vs Constructor

---
+++ 1. JS_Stack Learner/51. JS Object Literal vs Constructor/obj.js
@@ -0,0 +1,31 @@
+// let obj = {}
+// obj.x = 9
+// console.log(obj)
+
+
+
+// ----------------Object Literal.
+// let point = { 
+//     x : 3,
+//     y : 6
+// }
+// point.z = 9
+
+// console.log(point)
+
+
+
+
+//-----------------Object Constructor.
+// let obj = Object()
+// obj.a = 6
+// console.log(obj)
+
+// let obj = new Object()
+// obj.b = 9
+// console.log(obj)
+
+
+
+
+
