e0d4902f169d277bea683a136b727f210b4aeb47
bobo mallick <mallickbobo@gmail.com>
2021-08-15 13:58:19 +0530

    simple method obj comparing

--- 1. JS_Stack Learner/56. comparing two objects/objComparing.js
+++ 1. JS_Stack Learner/56. comparing two objects/objComparing.js
@@ -1,3 +1,21 @@
+// let obj1 = {
+//     a : 10,
+//     b : 20
+// }
+
+// let obj2 = {
+//     a : 10,
+//     b : 20
+// }
+
+// if (obj1.a == obj2.a && obj1.b == obj2.b){
+//     console.log(true)    
+// }
+
+
+
+
+
 let obj1 = {
     a : 10,
     b : 20
@@ -8,6 +26,4 @@ let obj2 = {
     b : 20
 }
 
-if (obj1.a == obj2.a && obj1.b == obj2.b){
-    console.log(true)    
-}
+console.log(JSON.stringify(obj1) === JSON.stringify(obj2));
