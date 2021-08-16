3781746f61a1256949caff5da5388d0fdef66446
bobo mallick <mallickbobo@gmail.com>
2021-08-16 09:21:12 +0530

    sum of array with for loop and function

---
+++ 1. JS_Stack Learner/62. Function arguments and parameters in JS/arguments.js
@@ -0,0 +1,24 @@
+// function add (a, b){
+//     let result = a + b
+//     console.log(result)
+// }
+// add(30, 90)
+// add(9, 60)
+
+
+let arr1 = [1, 2, 3]
+let arr2 = [4, 5, 6, 7]
+let arr3 = [8, 9, 10, 11]
+
+let sum = 0
+
+function sumOfArrays(arr){
+    for (let i = 0; i < arr.length; i++){
+        sum += arr[i]
+    }
+    console.log(sum)
+}
+
+sumOfArrays(arr1)
+sumOfArrays(arr2)
+sumOfArrays(arr3)
