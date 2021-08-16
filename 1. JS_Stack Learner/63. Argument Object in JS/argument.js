99f1a0c0f6a14173b7a12fdee4d2e759e8552fe1
bobo mallick <mallickbobo@gmail.com>
2021-08-16 11:21:31 +0530

    argument function obj in JS

--- 1. JS_Stack Learner/63. Argument Object in JS/argument.js
+++ 1. JS_Stack Learner/63. Argument Object in JS/argument.js
@@ -16,3 +16,13 @@
 // test(30, 60, 99)
 // test(3, 5, 6, 9, 4, 2, 33)
 // test (9, 6)
+
+function addAll (){
+    let sum = 0
+    for(let i=0; i < arguments.length; i++){
+        sum += arguments[i];
+    }
+    console.log(sum)
+}
+addAll(5, 7, 10)
+addAll(9, 6, 99, 30)
