6d711744760e8f694b191c9a8ec1c9528b1a52e7
bobo mallick <mallickbobo@gmail.com>
2021-08-16 11:16:00 +0530

    argument obj in JS

---
+++ 1. JS_Stack Learner/63. Argument Object in JS/argument.js
@@ -0,0 +1,18 @@
+// function test (a, b, c){
+//     for(var i=0; i < arguments.length; i++){
+//         console.log(arguments[i]);
+//     }
+// }
+// test(30, 60, 99)
+// test(7, 9, 4)
+
+
+
+// function test ( ){
+//     for(var i=0; i < arguments.length; i++){
+//         console.log(arguments[i]);
+//     }
+// }
+// test(30, 60, 99)
+// test(3, 5, 6, 9, 4, 2, 33)
+// test (9, 6)
