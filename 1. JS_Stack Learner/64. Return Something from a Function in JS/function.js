376b7503e6797493465d66928a87267aa4976496
bobo mallick <mallickbobo@gmail.com>
2021-08-19 11:54:49 +0530

    function return

--- 1. JS_Stack Learner/64. Return Something from a Function in JS/function.js
+++ 1. JS_Stack Learner/64. Return Something from a Function in JS/function.js
@@ -1,1 +1,10 @@
 // Return Something from a Function in JS
+function addAll(){
+    let sum = 0
+    for(let i = 0; i < arguments.length; i++){
+        sum += arguments[i]
+    }
+    return sum
+}
+let result = addAll(3, 6, 9)
+console.log(result)
