a24d3bcdfa73ce83d49f56156e3fde338dfec675
bobo mallick <mallickbobo@gmail.com>
2021-08-20 14:45:40 +0530

    done

--- 1. JS_Stack Learner/64. Return Something from a Function in JS/function.js
+++ 1. JS_Stack Learner/64. Return Something from a Function in JS/function.js
@@ -1,10 +1,21 @@
 // Return Something from a Function in JS
-function addAll(){
-    let sum = 0
-    for(let i = 0; i < arguments.length; i++){
-        sum += arguments[i]
+
+
+// function addAll(){
+//     let sum = 0
+//     for(let i = 0; i < arguments.length; i++){
+//         sum += arguments[i]
+//     }
+//     return sum
+// }
+// let result = addAll(3, 6, 9)
+// console.log(result)
+
+function person(name, email){
+    return{
+        Name: name,
+        Email: email
     }
-    return sum
 }
-let result = addAll(3, 6, 9)
-console.log(result)
+let p1 = person('Keya', 'keyamallick551@gmail.com')
+console.log(p1)
