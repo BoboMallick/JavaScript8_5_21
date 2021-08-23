a04f9652bb5f9ac8f0e3b6eeff4b1d814ec15013
bobo mallick <mallickbobo@gmail.com>
2021-08-23 23:36:32 +0530

    done

--- 1. JS_Stack Learner/66.  Inner Function in JS/InnerFunction.js
+++ 1. JS_Stack Learner/66.  Inner Function in JS/InnerFunction.js
@@ -1,7 +1,37 @@
-function somthing(greet, name){
-    function sayHi(){
-        console.log(greet, name);
+// function somthing(greet, name){
+//     function sayHi(){
+//         console.log(greet, name);
+//     }
+//     sayHi()
+// }
+// somthing('Good Morning', 'Keya Mallick')
+
+
+
+// function something(greet, name){
+//     function getFirstName(){
+//         if(name){
+//             return name.split(' ')[0]
+//         }
+//     }
+
+//     let message = greet + ' ' + getFirstName()
+//     console.log(message)
+// }
+// something('Good morning', 'Keya Mallick')
+
+
+
+function something(greet, name){
+    function getFirstName(){
+        if(name){
+            return name.split(' ')[0]
+        }else{
+            return ''
+        }
     }
-    sayHi()
+
+    let message = greet + ' ' + getFirstName()
+    console.log(message)
 }
-somthing('Good Morning', 'Keya Mallick')
+something('Good morning')
